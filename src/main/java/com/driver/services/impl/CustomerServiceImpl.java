90ef53a13faa1cd25f67101051e1dfd73d6bd332
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-02-26 14:04:46 +0530

    Check 3

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -174,7 +174,7 @@ public class CustomerServiceImpl implements CustomerService {
 		Driver driver = tripBooking.getDriver();
 		tripBooking.setBill(driver.getCab().getPerKmRate() * tripBooking.getDistanceInKm());
 		driver.getCab().setAvailable(true);//journey completed, so cab is available for upcoming customer
-//		driverRepository2.save(driver);//saved parent
-		tripBookingRepository2.save(tripBooking);//saved child
+		driverRepository2.save(driver);//saved parent
+//		tripBookingRepository2.save(tripBooking);//saved child
 	}
 }
